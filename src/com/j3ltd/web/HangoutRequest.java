295756e160c85a124d6271961533060e9cbfafde
Monai Thangsuphanich <monai.thang@gmail.com>
2013-01-10 22:21:09 +0700

    Fix RxList now it insert data successfully issues: MedicineList in MedicineList

--- src/com/j3ltd/web/HangoutRequest.java
+++ src/com/j3ltd/web/HangoutRequest.java
@@ -2,11 +2,9 @@ package com.j3ltd.web;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -30,146 +28,7 @@ public class HangoutRequest extends HttpServlet{
 	Prescription prescription;
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException,UnknownHostException{
 		RxList = new MedicineList();
-		tempRxList = new List<Medicine>() {
-			
-			@Override
-			public <T> T[] toArray(T[] a) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Object[] toArray() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public List<Medicine> subList(int fromIndex, int toIndex) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public int size() {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-			
-			@Override
-			public Medicine set(int index, Medicine element) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public boolean retainAll(Collection<?> c) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-			@Override
-			public boolean removeAll(Collection<?> c) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-			@Override
-			public Medicine remove(int index) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public boolean remove(Object o) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-			@Override
-			public ListIterator<Medicine> listIterator(int index) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public ListIterator<Medicine> listIterator() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public int lastIndexOf(Object o) {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-			
-			@Override
-			public Iterator<Medicine> iterator() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public boolean isEmpty() {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-			@Override
-			public int indexOf(Object o) {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-			
-			@Override
-			public Medicine get(int index) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public boolean containsAll(Collection<?> c) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-			@Override
-			public boolean contains(Object o) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-			@Override
-			public void clear() {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public boolean addAll(int index, Collection<? extends Medicine> c) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-			@Override
-			public boolean addAll(Collection<? extends Medicine> c) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-			@Override
-			public void add(int index, Medicine element) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public boolean add(Medicine e) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-		};
+		tempRxList = new ArrayList<Medicine>();
 		prescription = new Prescription();
 		prescription.setPatientID(request.getParameter("PatientID").toString());
 		prescription.setDoctorID(request.getParameter("DoctorID").toString());
@@ -190,6 +49,7 @@ public class HangoutRequest extends HttpServlet{
 			i++;
 		}
 		RxList.setMedicineList(tempRxList);
+		prescription.setMedicineList(RxList);
 		//Prepare Morphia Framework
 		System.out.println("Setting up MongoDB...");
 		Mongo mongo = new Mongo("localhost",27017);
